63dd7a86e0f801e408368f3979f348645cabd70c
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-08-19 18:58:03 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Show.destroy_all
+
+
+Show.create(name: "Schitt's Creek", network: "Neftlix", day: "Friday", rating: 5, season: "5")
+
+Show.create(name: "Handsmaid's tail", network: "Hulu", day: "Wednesday", rating: 5, season: "3")
+
+Show.create(name: "Younger", network: "Hulu", day: "Monday", rating: 3, season: "6")
